5244755fdc57a9f74fca2504f16907b2cde68a5e
faraz <farazgul521@gmail.com>
2022-07-22 20:04:05 +0500

    latest

---
+++ dist/6-es2015.133c71bd95cdc8f972c2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{mHm1:function(n,t,o){"use strict";o.d(t,"a",function(){return c}),o("b1EX");var r,e=o("8Y7J"),s=o("SVse");let c=(()=>{let n=r=class{static forRoot(){return{ngModule:r,providers:[]}}};return n.\u0275mod=e.Ub({type:n}),n.\u0275inj=e.Tb({factory:function(t){return new(t||n)},imports:[[s.c]]}),n})()}}]);
